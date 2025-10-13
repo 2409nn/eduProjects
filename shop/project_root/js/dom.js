16d1d202931522a3e63131075d2da3d1af76b445
Iskanderious <A.iskander07@gmail.com>
2025-10-13 19:01:36 +0500

    исправил баги

--- shop/project_root/js/dom.js
+++ shop/project_root/js/dom.js
@@ -1,22 +1,73 @@
-// ====== BURGER ======
+function calcPrice(priceElem, amount, operation=null) {
 
-class Burger {
-    constructor() {
-        this.item = document.querySelector("#header__burger-menu");
-        this.icon = document.querySelector(".header__burger-pic");
-        this.menu = document.querySelector(".header__burger-list");
+    let priceValue = Number(priceElem.innerText.match(/\d+/g));
+    let total;
+
+    switch (operation) {
+        case null:
+            total = Number(amount) * Number(priceValue);
+            break;
+
+        case "plus":
+            total = priceValue + (Number(priceValue / (Number(amount) - 1)));
+            break;
+
+        case "minus":
+            total = priceValue - (Number(priceValue) / (Number(amount) + 1));
     }
 
-    callMenu() {
-        this.menu.classList.toggle("active");
-        this.icon.classList.toggle("active");
+    priceElem.innerText = total;
+    return total;
+}
+
+function checkoutRegTable(optionalInputs, switchTab) {
+    if (optionalInputs.classList.contains("active")) {
+        switchTab.innerText = "Уже есть аккаунт";
+    }
+
+    else {
+        switchTab.innerText = "Еще нет аккаунта";
     }
 }
 
-const burger = new Burger();
-burger.icon.addEventListener("click", (e) => {
-    burger.callMenu();
-})
+// ===== BURGER =====
+
+try {
+    class Burger {
+        constructor() {
+            this.item = document.querySelector("#header__burger-menu");
+            this.icon = document.querySelector(".header__burger-pic");
+            this.menu = document.querySelector(".header__burger-list");
+        }
+
+        callMenu() {
+            this.menu.classList.toggle("active");
+            this.icon.classList.toggle("active");
+        }
+    }
+
+    const burger = new Burger();
+    burger.icon.addEventListener("click", (e) => {
+        burger.callMenu();
+    })
+
+} catch (e) {
+    console.log("burger menu not found");
+}
+
+// ===== SWITCH THEME =====
+
+const toggle = document.getElementById('toggle');
+
+toggle.addEventListener('change', () => {
+    if (toggle.checked) {
+        console.log('Включено');
+        document.body.style.backgroundColor = '#d1ffd6';
+    } else {
+        console.log('Выключено');
+        document.body.style.backgroundColor = '#f0f0f0';
+    }
+});
 
 // ===== MODAL =====
 
@@ -54,59 +105,60 @@ catch (e) {
     console.log('modal not found')
 }
 
-function calcPrice(priceElem, amount, operation=null) {
+// ===== counter ======
 
-    let priceValue = Number(priceElem.innerText.match(/\d+/g));
-    let total;
+try {
+    document.querySelectorAll(".counter").forEach(counter => {
+        let minusButton = counter.querySelector(".counter-minus");
+        let plusButton = counter.querySelector(".counter-plus");
+        let amount = counter.querySelector(".counter-amount");
+        let amountValue = Number(amount.innerText);
+        const product = counter.closest(".cProducts__product");
+        const priceElement = product.querySelector(".price");
 
-    switch (operation) {
-        case null:
-            total = Number(amount) * Number(priceValue);
-            break;
+        const MIN = 1;
+        const MAX = 99;
 
-        case "plus":
-            total = priceValue + (Number(priceValue / (Number(amount) - 1)));
-            break;
+        minusButton.addEventListener("click", (e) => {
 
-        case "minus":
-            total = priceValue - (Number(priceValue) / (Number(amount) + 1));
-    }
+            if (amountValue > MIN) {
+                amountValue -= 1;
+                amount.innerText = amountValue;
 
-    priceElem.innerText = total;
-    return total;
+                calcPrice(priceElement, amountValue, "minus");
+            }
+
+        })
+
+        plusButton.addEventListener("click", (e) => {
+
+            if (amountValue < MAX) {
+                amountValue += 1;
+                amount.innerText = amountValue;
+
+                calcPrice(priceElement, amountValue, "plus");
+            }
+        })
+
+    })
+
+} catch (e) {
+    console.log('counter not found')
 }
 
-// ===== counter ======
-document.querySelectorAll(".counter").forEach(counter => {
-    let minusButton = counter.querySelector(".counter-minus");
-    let plusButton = counter.querySelector(".counter-plus");
-    let amount = counter.querySelector(".counter-amount");
-    let amountValue = Number(amount.innerText);
-    const product = counter.closest(".cProducts__product");
-    const priceElement = product.querySelector(".price");
+// ===== registration switch tab =====
 
-    const MIN = 1;
-    const MAX = 99;
+try {
 
-    minusButton.addEventListener("click", (e) => {
+    const switchTab = document.querySelector(".reg__switch-tab");
+    const optionalInputs = document.querySelector(".reg__optional-inputs");
 
-        if (amountValue > MIN) {
-            amountValue -= 1;
-            amount.innerText = amountValue;
+    checkoutRegTable(optionalInputs, switchTab);
 
-            calcPrice(priceElement, amountValue, "minus");
-        }
+    switchTab.addEventListener("click", (e) => {
+        optionalInputs.classList.toggle("active");
 
+        checkoutRegTable(optionalInputs, switchTab);
     })
 
-    plusButton.addEventListener("click", (e) => {
-
-        if (amountValue < MAX) {
-            amountValue += 1;
-            amount.innerText = amountValue;
-
-            calcPrice(priceElement, amountValue, "plus");
-        }
-    })
-
-});
+} catch (e) {}
